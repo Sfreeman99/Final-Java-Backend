c74533db2a7338ae378b780e4a7c35227a3be99b
Shedlia Freeman <sfreeman@basecampcodingacademy.org>
2018-03-27 20:29:25 -0500

    Class that helps login a user

---
+++ src/main/java/com/example/backend/LoginUser.java
@@ -0,0 +1,12 @@
+package com.example.backend;
+
+public class LoginUser {
+    public String username;
+    public String password;
+
+    public LoginUser(){}
+    public LoginUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
