892bccaa547ee64ffd87480f78019f36be7db398
Shedlia Freeman <sfreeman@basecampcodingacademy.org>
2018-04-08 15:41:20 -0500

    Made depositing and withdrawing with you account available through the bank.

--- src/main/java/com/example/backend/model/Transaction.java
+++ src/main/java/com/example/backend/model/Transaction.java
@@ -4,13 +4,13 @@ public class Transaction {
     public String username;
     public int amount;
     public int userId;
-    public int otherId;
+    public int otherId = 7;
 
     public Transaction(){}
-    public Transaction(String username, int amount, int userId, int otherId) {
+    public Transaction(String username, int amount, int userId) {
         this.username = username;
         this.amount = amount;
         this.userId = userId;
-        this.otherId = otherId;
+        this.otherId = 7;
     }
 }
